c476c056445c30231881dc5086699e29f8c37d21
Dmitrii Vladimirov | Olga Nogtich <49186663+mirozdanie6v@users.noreply.github.com>
2026-09-15 02:56:53 +0700

    Load source parity UI overrides

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -5,6 +5,7 @@ import AppV2 from './AppV2';
 import './styles.css';
 import './tour-pages.css';
 import './source-parity.css';
+import './parity-fixes.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
